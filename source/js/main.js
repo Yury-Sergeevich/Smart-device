d7e3e007b75776499f04d83c73438d3813cde561
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-10-26 19:23:14 +0300

    Add footer

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,4 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 import {openModal} from './modules/modals/modal';
 // ---------------------------------
 
@@ -17,7 +16,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
   });
 });
 

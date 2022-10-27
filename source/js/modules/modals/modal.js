534823145cc330ebb8ca26d0fac8a2d2b8582f0c
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-10-27 16:13:13 +0300

    Add murkup footer in style desctop and tablet

--- source/js/modules/modals/modal.js
+++ source/js/modules/modals/modal.js
@@ -4,7 +4,7 @@ const modalWindow = document.querySelector('.modal-container');
 function openModal() {
   headerButton.addEventListener('click', function () {
     if (modalWindow.classList.contains('modal-container--close')) {
-      modalWindow.classList.add('modal-container--close');
+      modalWindow.classList.remove('modal-container--close');
     } else {
       modalWindow.classList.add('modal-container--close');
     }

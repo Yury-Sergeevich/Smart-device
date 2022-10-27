0ea02c408e79dbac4c233d9886cb7980fcbdc0d6
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-10-27 17:32:36 +0300

    Fix script modal, close

--- source/js/modules/modals/modal.js
+++ source/js/modules/modals/modal.js
@@ -1,5 +1,7 @@
 const headerButton = document.querySelector('.header__button');
+const modalCloseButton = document.querySelector('.modal__close-button');
 const modalWindow = document.querySelector('.modal-container');
+const modalContainer = document.querySelector('.modal-container__modal');
 
 function openModal() {
   headerButton.addEventListener('click', function () {
@@ -8,6 +10,18 @@ function openModal() {
     } else {
       modalWindow.classList.add('modal-container--close');
     }
+  });
+
+  modalCloseButton.addEventListener('click', function () {
+    modalWindow.classList.add('modal-container--close');
+  });
+
+  modalWindow.addEventListener('click', function () {
+    modalWindow.classList.add('modal-container--close');
+  });
+
+  modalContainer.addEventListener('click', function (event) {
+    event.stopPropagation();
   });
 }
 

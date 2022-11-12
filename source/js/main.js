163962bef25fcd0b6f2c5863f5c43d2f9b603876
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-11-12 18:58:23 +0300

    Add accordion

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {openModal} from './modules/modals/modal';
-import {accordeonInit} from './modules/accordeon';
+import {initAccordion, accContent} from './modules/accordeon';
 import {maskPhone} from './modules/mask-phone';
 import {scrollTarget, handleButtonclick} from './modules/sroll';
 import {openAboutDetail} from './modules/about';
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // ---------------------------------
   maskPhone();
   openModal();
-  accordeonInit();
+  initAccordion(accContent);
   iosVhFix();
   openAboutDetail();
 
--- source/js/modules/accordeon.js
+++ source/js/modules/accordeon.js
@@ -1,11 +1,31 @@
-const accordeonButton = document.querySelectorAll('.nav-site__acardeon-button');
-const accordeonContent = document.querySelectorAll('.nav-site__acardeon-content');
-const accNojs = document.querySelectorAll('.nav-site__accordeon-no-js');
+function initAccordion() {
+  const accContent = document.querySelectorAll('.accordion__body');
+  const accButtons = document.querySelectorAll('.accordion__btn');
 
-function accordeonInit() {
-  accNojs.forEach((item) => {
-    item.classList.remove('nav-site__accordeon-no-js');
+  accContent.forEach((item) => item.classList.remove('no-js'));
+
+  accButtons.forEach((item) => {
+    item.addEventListener('click', () => {
+      let content = item.nextElementSibling;
+      if (content.classList.contains('open') && item.classList.contains('accordion__btn--open')) {
+        accContent.forEach(function (el) {
+          el.classList.remove('open');
+        });
+        accButtons.forEach(function (el) {
+          el.classList.add('accordion__btn--open');
+        });
+      } else {
+        accContent.forEach(function (el) {
+          el.classList.remove('open');
+        });
+        accButtons.forEach(function (el) {
+          el.classList.remove('accordion__btn--open');
+        });
+        item.classList.add('accordion__btn--open');
+        content.classList.add('open');
+      }
+    });
   });
 }
 
-export {accordeonInit};
+export {initAccordion};

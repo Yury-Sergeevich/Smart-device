0bf9b8bd76d6a5325048de0700beb3245ee83658
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-11-16 12:39:55 +0300

    Fix eventlistener in button from section About

--- source/js/modules/about.js
+++ source/js/modules/about.js
@@ -3,15 +3,17 @@ const aboutDetail = document.querySelector('.about__detail');
 const buttonText = document.querySelector('.about__button-text');
 
 function openAboutDetail() {
-  aboutButton.addEventListener('click', function () {
-    if (aboutDetail.classList.contains('about__detail--close')) {
-      aboutDetail.classList.remove('about__detail--close');
-      buttonText.textContent = 'Свернуть';
-    } else {
-      aboutDetail.classList.add('about__detail--close');
-      buttonText.textContent = 'Подробнее';
-    }
-  });
+  if (aboutButton) {
+    aboutButton.addEventListener('click', function () {
+      if (aboutDetail.classList.contains('about__detail--close')) {
+        aboutDetail.classList.remove('about__detail--close');
+        buttonText.textContent = 'Свернуть';
+      } else {
+        aboutDetail.classList.add('about__detail--close');
+        buttonText.textContent = 'Подробнее';
+      }
+    });
+  }
 }
 
 export {openAboutDetail};

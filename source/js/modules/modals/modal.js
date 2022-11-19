ee67a732d4d0c0d7c7bb8d1c791d1a19985eb73a
Yury Kravchenko <yury.kravchenko.1987@gmail.com>
2022-11-19 21:45:17 +0300

    Add focusable

--- source/js/modules/modals/modal.js
+++ source/js/modules/modals/modal.js
@@ -2,19 +2,50 @@ const headerButton = document.querySelector('.header__button');
 const modalCloseButton = document.querySelector('.modal__close-button');
 const modalWindow = document.querySelector('.modal-container');
 const modalContainer = document.querySelector('.modal-container__modal');
-const nameInput = document.getElementById('name');
+const focusableElements = 'input, textarea, button, [tabindex]:not([tabindex="-1"])';
+const firstFocusableElement = document.getElementById('name');
+const focusableContent = modalWindow.querySelectorAll(focusableElements);
+const lastFocusableElement = focusableContent[focusableContent.length - 1];
 
-function openModal() {
+
+function escClose() {
   document.addEventListener('keydown', (e) => {
     if (e.key === 'Escape') {
       modalWindow.classList.add('modal-container--close');
     }
   });
+}
+
+function focusable() {
+  document.addEventListener('keydown', function (e) {
+    let isTabPressed = e.key === 'Tab' || e.key === 9;
+
+    if (!isTabPressed) {
+      return;
+    }
+
+    if (e.shiftKey) {
+      if (document.activeElement === firstFocusableElement) {
+        lastFocusableElement.focus();
+        e.preventDefault();
+      }
+    } else {
+      if (document.activeElement === lastFocusableElement) {
+        firstFocusableElement.focus();
+      }
+    }
+  });
+}
+
+function openModal() {
+  escClose();
+  focusable();
+
   headerButton.addEventListener('click', function () {
     document.body.style.overflow = 'hidden';
     if (modalWindow.classList.contains('modal-container--close')) {
       modalWindow.classList.remove('modal-container--close');
-      nameInput.focus();
+      firstFocusableElement.focus();
     } else {
       modalWindow.classList.add('modal-container--close');
     }
@@ -31,6 +62,7 @@ function openModal() {
   });
 
   modalContainer.addEventListener('click', (event) => {
+    lastFocusableElement.focus();
     event.stopPropagation();
   });
 }
